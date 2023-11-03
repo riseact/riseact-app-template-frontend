289bac17286c809e13b2f676473c4145b037f484
Jonathan Mataloni <jo.mataloni@gmail.com>
2023-11-03 12:58:51 +0100

    disable cache

--- src/config/network.ts
+++ src/config/network.ts
@@ -4,4 +4,14 @@ import urlJoin from 'url-join';
 export const gqlClient = new ApolloClient({
   uri: urlJoin(location.origin, '/graphql'),
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'ignore',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    },
+  },
 });
